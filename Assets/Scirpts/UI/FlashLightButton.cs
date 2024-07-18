3af0374dba054dc299e59f48b16b26402e0f67e4
flslin <84002788+flslin@users.noreply.github.com>
2024-07-18 11:08:55 +0900

    FNAF-45 손전등 기능 구현
    손전등 버튼 클릭 시 이미지 변경 기능 구현

--- Assets/Scirpts/UI/FlashLightButton.cs
+++ Assets/Scirpts/UI/FlashLightButton.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlashLightButton : MonoBehaviour
 {
+    [SerializeField] private Image flashButton;
+    [SerializeField] private Sprite onButton;
+    [SerializeField] private Sprite offButton;
 
-
-    void Start()
+    public void ButtonClick()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        if (flashButton.sprite == offButton)
+        {
+            Debug.Log("ÇÃ·¡½Ã ÄÑÁü");
+            flashButton.sprite = onButton;
+        }
+        else
+        {
+            Debug.Log("ÇÃ·¡½Ã ²¨Áü");
+            flashButton.sprite = offButton;
+        }
     }
 }
