a0722261946cfa4c7e0c3b979b60e32b2cc9b3f7
flslin <84002788+flslin@users.noreply.github.com>
2024-07-18 15:55:52 +0900

    FNAF-45 손전등 기능 구현
    손전등 버튼 클릭 시 Global Volum의 Vignette 수치와 Shadows Midtones Highlights의 Shadows 수치를 변경하도록 했다.

--- Assets/Scirpts/UI/FlashLightButton.cs
+++ Assets/Scirpts/UI/FlashLightButton.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class FlashLightButton : MonoBehaviour
@@ -8,18 +10,40 @@ public class FlashLightButton : MonoBehaviour
     [SerializeField] private Image flashButton;
     [SerializeField] private Sprite onButton;
     [SerializeField] private Sprite offButton;
+    [SerializeField] private Volume volume;
+    [SerializeField] private ShadowsMidtonesHighlights shadow;
+
+    private Vignette vignette;
+
+    private void Awake()
+    {
+        if (volume.profile.TryGet<Vignette>(out vignette) && volume.profile.TryGet<ShadowsMidtonesHighlights>(out shadow))
+        {
+            vignette.intensity.value = 0.6f;
+            shadow.shadows.SetValue(new Vector4Parameter(new Vector4(0, 0, 0, -0.6f)));
+        }
+    }
 
     public void ButtonClick()
     {
         if (flashButton.sprite == offButton)
         {
-            Debug.Log("ÇÃ·¡½Ã ÄÑÁü");
             flashButton.sprite = onButton;
+            if (volume.profile.TryGet<Vignette>(out vignette) && volume.profile.TryGet<ShadowsMidtonesHighlights>(out shadow))
+            {
+                vignette.intensity.value = 0.45f;
+                shadow.shadows.SetValue(new Vector4Parameter(new Vector4(0, 0, 0, 0.2f)));
+            }
         }
         else
         {
-            Debug.Log("ÇÃ·¡½Ã ²¨Áü");
             flashButton.sprite = offButton;
+
+            if (volume.profile.TryGet<Vignette>(out vignette) && volume.profile.TryGet<ShadowsMidtonesHighlights>(out shadow))
+            {
+                vignette.intensity.value = 0.6f;
+                shadow.shadows.SetValue(new Vector4Parameter(new Vector4(0, 0, 0, -0.6f)));
+            }
         }
     }
 }
