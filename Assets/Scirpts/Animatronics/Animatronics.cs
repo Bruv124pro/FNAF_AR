9ef916d430d161ce738c5f105f08f2ff9acf8f37
sungjin <sungjin2836@naver.com>
2024-07-23 15:25:43 +0900

    FNAF Glitch 씬에서 카메라뷰에 애니매트로닉스가 보일 경우에만 적용되게 추가

--- Assets/Scirpts/Animatronics/Animatronics.cs
+++ Assets/Scirpts/Animatronics/Animatronics.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections;
+using System.Reflection;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Animatronics : MonoBehaviour
 {
@@ -38,6 +42,8 @@ public class Animatronics : MonoBehaviour
 
     public Camera camera;
 
+    public Material glitchMaterial;
+
     public event Action OnSoundPlayFinished;
     public event Action OnVisibleFinished;
 
@@ -50,7 +56,7 @@ public class Animatronics : MonoBehaviour
     private float bodyAlpha;
     private float eyeAlpha;
 
-    void Start()
+    void Start()    
     {
         animator = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
@@ -62,7 +68,6 @@ public class Animatronics : MonoBehaviour
         isFinishCircleMove = false;
         alreadyinit = false;
     }
-
     public void SetVisible()
     {
         bodyShader.SetFloat("_Alpha", 0);
@@ -278,5 +283,16 @@ public class Animatronics : MonoBehaviour
         return visibleanimName;
     }
 
+    public void OnOffGlitchMaterial()
+    {
+        if (IsFindVisibleAnimatronics())
+        {
+            glitchMaterial.SetFloat("_Force", 10);    
+        }
+        else
+        {
+            glitchMaterial.SetFloat("_Force", 0);
+        }
+    }
 
 }
