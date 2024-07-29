0be747c8a0d1742f5f2e9fb642e7b91f742986d8
sungjin <sungjin2836@naver.com>
2024-07-29 17:58:26 +0900

    FNAF-95 맵상 애니매트로닉스 이동기능 구현

--- Assets/Scirpts/SelectView/TraceMapObject.cs
+++ Assets/Scirpts/SelectView/TraceMapObject.cs
@@ -5,16 +5,30 @@ using UnityEngine.UI;
 
 public class TraceMapObject : MonoBehaviour
 {
+    Rigidbody rb;
     [SerializeField] private GameObject traceButton;
     RectTransform rectTransform;
+    private Vector3 localPosOffset = Vector3.zero;
 
     void Start()
     {
         rectTransform = traceButton.GetComponent<RectTransform>();
+        StartCoroutine(MoveAnimatronicsButton());
     }
 
     void Update()
     {
-        rectTransform.position = transform.position;
+        rectTransform.position = transform.position + Vector3.back;
+    }
+
+    IEnumerator MoveAnimatronicsButton()
+    {
+        while (true)
+        {
+            float ranX = Random.Range(-2f, 2f);
+            float ranY = Random.Range(-2f, 2f);
+            transform.position = new Vector3(transform.position.x + ranX, transform.position.y + ranY, transform.position.z);
+            yield return new WaitForSeconds(4);
+        }
     }
 }
