b578889a99bb9d36a8daa19938c822f38571c207
flslin <84002788+flslin@users.noreply.github.com>
2024-07-30 14:10:28 +0900

    FNAF-94-Loading
    로딩 씬 구현

--- Assets/Scirpts/Manager/Managers.cs
+++ Assets/Scirpts/Manager/Managers.cs
@@ -12,17 +12,11 @@ public class Managers : MonoBehaviour
     public static Managers s_instance = null;
     public static Managers Instance { get { return s_instance; } }
 
-    //private static GameManager s_gameManager = new GameManager();
-    private static DataManager s_dataManager = new DataManager();
     private static UIManager s_uiManager = new UIManager();
     private static ResourceManager s_resourceManager = new ResourceManager();
-    private static SceneManagerEx s_sceneManager = new SceneManagerEx();
 
-    //public static GameManager Game { get { Init(); return s_gameManager; } }
-    public static DataManager Data { get { Init(); return s_dataManager; } }
     public static UIManager UI { get { Init(); return s_uiManager; } }
     public static ResourceManager Resource { get { Init(); return s_resourceManager; } }
-    public static SceneManagerEx Scene { get { Init(); return s_sceneManager; } }
 
     //public static string GetText(int id)
     //{
@@ -49,7 +43,6 @@ public class Managers : MonoBehaviour
             DontDestroyOnLoad(go);
 
             s_resourceManager.Init();
-            s_sceneManager.Init();
 
             Application.targetFrameRate = 60;
         }
--- Assets/Scirpts/UI/PopUp/UI_InGamePopup.cs
+++ Assets/Scirpts/UI/PopUp/UI_InGamePopup.cs
@@ -1,14 +1,33 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 using static Define;
 
 public class UI_InGamePopup : UI_Popup
 {
-    private ShockButton shock;
-    private FlashLightButton flash;
+    [SerializeField] private Image flashButton;
+    [SerializeField] private Sprite onButton;
+    [SerializeField] private Sprite offButton;
+    [SerializeField] private Volume volume;
+    [SerializeField] private Button shockButton;
+    [SerializeField] private Image shock;
+    [SerializeField] private Sprite shockImage;
+    [SerializeField] private Sprite shockBackgroundImage;
+    [SerializeField] private Slider coolTime;
+    [SerializeField] private int elapsedTime;
+    [SerializeField] private BatteryUse battery;
+    [SerializeField] private Animatronics animatronics;
+
+    private ShadowsMidtonesHighlights shadow;
+    private Vignette vignette;
+    public bool isFlashPressed { get; private set; }
+    public bool isShockPressed { get; private set; }
+
     private Slider batterySlider;
 
     enum Texts
@@ -41,13 +60,15 @@ public class UI_InGamePopup : UI_Popup
 
     public override bool Init()
     {
-        if(base.Init() == false)
+        if (base.Init() == false)
         {
             return false;
         }
 
-        shock = GetComponent<ShockButton>();
-        flash = GetComponent<FlashLightButton>();
+        if (volume.profile.TryGet<Vignette>(out vignette) && volume.profile.TryGet<ShadowsMidtonesHighlights>(out shadow))
+        {
+            VignetteValueChange("off");
+        }
 
         BindButton(typeof(Buttons));
         BindSlider(typeof(Sliders));
@@ -56,29 +77,192 @@ public class UI_InGamePopup : UI_Popup
         GetButton((int)Buttons.FlashLightButton).gameObject.BindEvent(OnClickFlashButton);
 
         batterySlider = GetSlider((int)Sliders.BatterySlider);
-        if(batterySlider != null)
+        if (batterySlider != null)
         {
             batterySlider.interactable = false;
         }
+        else
+        {
+            Debug.LogError("BatterySlider not found");
+        }
+
+        coolTime.interactable = false;
+        isShockPressed = false;
+        shock.sprite = shockImage;
+        elapsedTime = 100;
 
         return true;
     }
 
+    void Update()
+    {
+        if (battery.batteryAmount <= 0)
+        {
+            VignetteValueChange("off");
+        }
+
+        if (elapsedTime < 100)
+        {
+            shock.sprite = shockBackgroundImage;
+            elapsedTime += 1;
+            coolTime.value = elapsedTime;
+        }
+
+        if (elapsedTime == 100)
+        {
+            shock.sprite = shockImage;
+        }
+    }
+
     void OnClickShockButton()
     {
-        shock.ButtonClick();
-        if(_onClickShockButton != null)
+        if (battery.batteryAmount > 10 && elapsedTime == 100)
+        {
+            isShockPressed = true;
+            if (animatronics.isJumpState && animatronics.IsVisibleInMonitor())
+            {
+                animatronics.HitElecParticle(true);
+                animatronics.isHitElectronic = true;
+            }
+            else
+            {
+                animatronics.HitElecParticle(false);
+            }
+            elapsedTime = 0;
+            coolTime.value = elapsedTime;
+        }
+        else if (elapsedTime < 100 && isShockPressed)
+        {
+            isShockPressed = !isShockPressed;
+        }
+
+        if (_onClickShockButton != null)
         {
             _onClickShockButton.Invoke();
         }
+        Debug.Log("Shock button 클릭");
     }
 
     void OnClickFlashButton()
     {
-        flash.ButtonClick();
+        if (battery.batteryAmount > 0 && flashButton.sprite == offButton)
+        {
+            VignetteValueChange("on");
+        }
+        else
+        {
+            VignetteValueChange("off");
+        }
+
         if (_onClickFlashButton != null)
         {
             _onClickFlashButton.Invoke();
         }
+        Debug.Log("Flash button 클릭");
+    }
+
+    private void VignetteValueChange(string vignetteValue)
+    {
+        if (vignetteValue == "on")
+        {
+            isFlashPressed = true;
+            flashButton.sprite = onButton;
+
+            vignette.intensity.value = 0.5f;
+            shadow.shadows.SetValue(new Vector4Parameter(new Vector4(0, 0, 0, 0.25f)));
+        }
+        else if (vignetteValue == "off")
+        {
+            isFlashPressed = false;
+            flashButton.sprite = offButton;
+
+            vignette.intensity.value = 0.65f;
+            shadow.shadows.SetValue(new Vector4Parameter(new Vector4(0, 0, 0, -0.3f)));
+        }
     }
 }
+
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using static Define;
+
+//public class UI_InGamePopup : UI_Popup
+//{
+//    private ShockButton shock;
+//    private FlashLightButton flash;
+//    private Slider batterySlider;
+
+//    enum Texts
+//    {
+//        BatteryAmountText
+//    }
+
+//    enum Images
+//    {
+//        ShockImage,
+//        ShockCoolDownImage,
+//        FlashOnImage,
+//        FlashOffImage
+//    }
+
+//    enum Buttons
+//    {
+//        ShockAttackButton,
+//        FlashLightButton,
+//        CancleButton
+//    }
+
+//    enum Sliders
+//    {
+//        BatterySlider
+//    }
+
+//    Action _onClickFlashButton;
+//    Action _onClickShockButton;
+
+//    public override bool Init()
+//    {
+//        if(base.Init() == false)
+//        {
+//            return false;
+//        }
+
+//        shock = GetComponent<ShockButton>();
+//        flash = GetComponent<FlashLightButton>();
+
+//        BindButton(typeof(Buttons));
+//        BindSlider(typeof(Sliders));
+
+//        GetButton((int)Buttons.ShockAttackButton).gameObject.BindEvent(OnClickShockButton);
+//        GetButton((int)Buttons.FlashLightButton).gameObject.BindEvent(OnClickFlashButton);
+
+//        batterySlider = GetSlider((int)Sliders.BatterySlider);
+//        if(batterySlider != null)
+//        {
+//            batterySlider.interactable = false;
+//        }
+
+//        return true;
+//    }
+
+//    void OnClickShockButton()
+//    {
+//        shock.ButtonClick();
+//        if(_onClickShockButton != null)
+//        {
+//            _onClickShockButton.Invoke();
+//        }
+//    }
+
+//    void OnClickFlashButton()
+//    {
+//        flash.ButtonClick();
+//        if (_onClickFlashButton != null)
+//        {
+//            _onClickFlashButton.Invoke();
+//        }
+//    }
+//}
