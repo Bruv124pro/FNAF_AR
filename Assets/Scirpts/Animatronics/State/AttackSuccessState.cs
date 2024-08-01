f47de95a300093f647d9d3ef6a461c38987f6980
sungjin <sungjin2836@naver.com>
2024-08-01 13:20:38 +0900

    FNAF-112 인스펙터창으로 관리하던 Material들 스크립트에서 관리하게 수정

--- Assets/Scirpts/Animatronics/State/AttackSuccessState.cs
+++ Assets/Scirpts/Animatronics/State/AttackSuccessState.cs
@@ -18,7 +18,6 @@ public class AttackSuccessState : IState
         animatronics.flashButton.interactable = false;
         animatronics.shockButton.interactable = false;
         animatronics.GameOverOverlay();
-        //animatronics.GetComponentInChildren<Transform>().gameObject.SetActive(false);
         animatronics.transform.GetChild(1).gameObject.SetActive(false);
     }
 
